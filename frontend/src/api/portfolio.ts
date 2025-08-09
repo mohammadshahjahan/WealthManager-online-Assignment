0ee4d16cb29b822fbef18c14704196d495faee6c
Mohd Shahjahan <md.shahjahan786pubg@gmail.com>
2025-08-09 12:34:11 +0530

    added live backend service url

--- frontend/src/api/portfolio.ts
+++ frontend/src/api/portfolio.ts
@@ -1,7 +1,7 @@
 // src/api/portfolio.ts
 import axios from "axios";
 
-const API_BASE = "http://localhost:8080/api/portfolio";
+const API_BASE = "https://wealthmanager-online-assignment.onrender.com/api/portfolio";
 
 export interface Holding {
   symbol: string;
